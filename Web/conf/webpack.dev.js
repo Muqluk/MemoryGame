413404f612fb7bf068ab8e4d49a21767a8881135
muqluk@yahoo.com <muqluk@yahoo.com>
2019-08-22 22:44:24 -0500

    updated and corrected spinners

--- Web/conf/webpack.dev.js
+++ Web/conf/webpack.dev.js
@@ -10,7 +10,8 @@ module.exports = Merge(CommonConfig, {
     port: 9000,
     contentBase: path.join(process.cwd(), 'dist'), // static file location
     // host: 'localhost',
-    host: '10.20.104.25',
+    // host: '10.20.104.25',
+    host: '192.168.1.216',
     historyApiFallback: true, // true for index.html upon 404, object for multiple paths
     noInfo: false,
     stats: 'minimal',
